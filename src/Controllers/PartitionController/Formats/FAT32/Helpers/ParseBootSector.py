33a4466367465c1c29613f788bd294d2436eddb2
Livens <dangkhoah77@gmail.com>
2025-04-11 21:36:32 +0700

    fix: update ParseBootSector function parameters and return type for clarity

--- src/Controllers/PartitionController/Formats/FAT32/Helpers/ParseBootSector.py
+++ src/Controllers/PartitionController/Formats/FAT32/Helpers/ParseBootSector.py
@@ -1,29 +1,29 @@
 import io as io
 
 
-def ParseBootSector(metadata: io.BufferedReader) -> dict:
+def ParseBootSector(volume: io.BufferedReader) -> dict[str, int]:
     """Parse the boot sector of a FAT32 partition.
 
     ### Parameters
-    - **metadata** `io.BufferedReader`: The file-like object containing the FAT32 partition data.
+    - **volume** `BufferedReader`: The volume containing the FAT32 partition data.
 
     ### Returns
     - `dict`: A dictionary containing FAT32 metadata including:
         - **bytesPerSector** `int`: Number of bytes per sector.
-        - **sectorsPerCluster** `int`: Number of sectors per cluster.
+        - **sectorsPerCluster** `bytes`: Number of sectors per cluster.
         - **reservedSectors** `int`: Number of reserved sectors.
         - **numFats** `int`: Number of FATs.
         - **sectorsPerFat** `int`: Number of sectors per FAT.
         - **rootCluster** `int`: The first cluster of the root directory.
     """
-    metadata.seek(0)
-    boot = metadata.read(512)
+    volume.seek(0)
+    boot = volume.read(512)
 
     return {
         "bytesPerSector": int.from_bytes(boot[11:13], "little"),
-        "sectorsPerCluster": boot[13],
+        "sectorsPerCluster": int.from_bytes(boot[13], "little"),
         "reservedSectors": int.from_bytes(boot[14:16], "little"),
-        "numFats": boot[16],
+        "numFats": int.from_bytes(boot[16], "little"),
         "sectorsPerFat": int.from_bytes(boot[36:40], "little"),
         "rootCluster": int.from_bytes(boot[44:48], "little"),
     }
